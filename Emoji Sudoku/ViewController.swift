cc3a4e1e0158c011d214a7619c3df46bcddf881e
Cal Stephens <calstephens98@gmail.com>
2015-05-02 23:56:37 -0400

    Initial Commit

---
+++ Emoji Sudoku/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Emoji Sudoku
+//
+//  Created by Cal on 5/2/15.
+//  Copyright (c) 2015 Cal Stephens. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Emoji SudokuTests/Emoji_SudokuTests.swift
@@ -0,0 +1,36 @@
+//
+//  Emoji_SudokuTests.swift
+//  Emoji SudokuTests
+//
+//  Created by Cal on 5/2/15.
+//  Copyright (c) 2015 Cal Stephens. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Emoji_SudokuTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
